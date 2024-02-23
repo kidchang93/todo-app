1f5b5fea5b07dc6e10e3e3f713b6b823938662e8
kidchang93 <lck930721@gmail.com>
2024-02-23 11:39:47 +0900

    TodoList 기능 구현하기

--- src/App.js
+++ src/App.js
@@ -1,13 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 import TodoTemplate from "./components/TodoTemplate";
 import TodoInsert from "./components/TodoInsert";
-import TodoListItem from "./components/TodoListItem";
+import TodoList from "./components/TodoList";
 
 const App = () => {
+	const [todos, setTodos] = useState([
+		{
+			id: 1,
+			text: "일정1",
+			checked: true,
+		},
+		{
+			id: 2,
+			text: "일정2",
+			checked: true,
+		},
+		{
+			id: 3,
+			text: "일정3",
+			checked: false,
+		},
+	]);
 	return (
 		<TodoTemplate>
 			<TodoInsert />
-			<TodoListItem />
+			<TodoList todos={todos} />
 		</TodoTemplate>
 	);
 };
