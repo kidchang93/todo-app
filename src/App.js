35431e83ba3d9081180675950a16c595793fc731
kidchang93 <lck930721@gmail.com>
2024-02-23 17:41:05 +0900

    컴포넌트 성능 최적화 적용

--- src/App.js
+++ src/App.js
@@ -16,35 +16,26 @@ const App = () => {
 	const [todos, setTodos] = useState(createBulkTodos);
 	// console.log(todos);
 	const nextId = useRef(2501); // id 관리용
-	const onInsert = useCallback(
-		(text) => {
-			const nextTodo = { id: nextId.current, text: text, checked: false };
-			setTodos(todos.concat(nextTodo));
-			nextId.current = nextId.current + 1;
-		},
-		[todos]
-	);
+	const onInsert = useCallback((text) => {
+		const nextTodo = { id: nextId.current, text: text, checked: false };
+		setTodos((todos) => todos.concat(nextTodo));
+		nextId.current = nextId.current + 1;
+	}, []);
 	// TodoListItem remove button click => id
-	const onRemove = useCallback(
-		(id) => {
-			setTodos(todos.filter((todo) => todo.id !== id));
-			console.log(todos);
-		},
-		[todos]
-	);
+	const onRemove = useCallback((id) => {
+		setTodos((todos) => todos.filter((todo) => todo.id !== id));
+		console.log(todos);
+	}, []);
 
 	// todos 배열 객체 수정하기
 	// ...todo => 전체 다 까봐야되니까 ( 데이터가 많으면 성능 저하됨 ) 다른 방법 구상해야됨.
-	const onToggle = useCallback(
-		(id) => {
-			setTodos(
-				todos.map((todo) =>
-					todo.id === id ? { ...todo, checked: !todo.checked } : todo
-				)
-			);
-		},
-		[todos]
-	);
+	const onToggle = useCallback((id) => {
+		setTodos((todos) =>
+			todos.map((todo) =>
+				todo.id === id ? { ...todo, checked: !todo.checked } : todo
+			)
+		);
+	}, []);
 	return (
 		<TodoTemplate>
 			<TodoInsert onInsert={onInsert} />
